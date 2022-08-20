d7a4a1de4583a96a9e9096e7e29c530558663390
Doohan <doohanaditya@yahoo.co.id>
2022-08-20 15:13:34 +0700

    chore (axios): install axios and set globally axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from './plugins/axios';
 import './index.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  axios,
   render: (h) => h(App),
 }).$mount('#app');
