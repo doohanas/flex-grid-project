b52c64e0f26f5e5ec78f928f9f1bd25e2834b822
Doohan AS <79241754+doohanas@users.noreply.github.com>
2022-08-21 14:48:54 +0700

    feat (button): create button component

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button
+      class="button"
+      v-bind="{...$props,...$attrs}"
+      v-on="$listeners"
+    >
+      {{ label }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    variant: {
+      type: String,
+      default: 'primary',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'button',
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.button {
+  @apply p-2 rounded-md shadow-md bg-green-800 text-center text-[14px] leading-[23px]
+  text-yellow-50;
+}
+</style>
