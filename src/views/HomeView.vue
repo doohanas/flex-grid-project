be90a44422b25e331b5bd2ab348b84116cce3dab
Doohan <doohanaditya@yahoo.co.id>
2023-01-17 14:11:48 +0700

    chore: install vue draggable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="home">
+    Hello Vue Draggable
+    <draggable v-model="items" group="people" @start="drag=true" @end="drag=false">
+      <div v-for="element in items" :key="element.id">{{element.title}}</div>
+    </draggable>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import draggable from '@/plugins/draggable';
 
 export default {
-  name: 'HomeView',
+    name: "HomeView",
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    title: "Item 1",
+                },
+                {
+                    id: 2,
+                    title: "Item 2",
+                },
+                {
+                    id: 3,
+                    title: "Item 3",
+                },
+            ]
+        };
+    },
+    components: { draggable }
 };
 </script>
