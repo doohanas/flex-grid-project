06e65d1441f34ea8979ddfd7b1ff8a2db423679e
Doohan <doohanaditya@yahoo.co.id>
2023-01-17 15:21:09 +0700

    feat (table-draggable): create draggable for table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <template>
   <div class="home">
     Hello Vue Draggable
-    <draggable v-model="items" group="people" @start="drag=true" @end="drag=false">
+    
+    <!-- Lust Item Draggable -->
+
+    <draggable v-model="items" >
       <div v-for="element in items" :key="element.id">{{element.title}}</div>
     </draggable>
+
+    <!-- Table Draggable -->
+    <section class="min-w-[500px] min-h-[500px]">
+
+      <table class="mx-auto">
+          <thead>    
+              <th class="pr-4" v-for="header in headers" :key="header" scope="col">
+                {{ header }}
+              </th>
+          </thead>
+     
+          <draggable v-model="lists" tag="tbody">
+            <tr v-for="item in lists" :key="item.judul">
+              <td>{{ item.id }}</td>
+              <td><img src="@/assets/drag.png" width="30" height="30" alt="">{{ item.judul }}</td>
+              <td>{{ item.preview }}</td>     
+              <td>{{ item.urutan }}</td>     
+              <td>{{ item.link }}</td>     
+              <td>{{ item.status }}</td>     
+              <td>{{ item.aksi }}</td>     
+            </tr>
+          </draggable>
+    
+      </table>
+     
+    </section>
   </div>
 </template>
 
@@ -27,8 +56,16 @@ export default {
                     id: 3,
                     title: "Item 3",
                 },
-            ]
-        };
+            ],
+            headers: ["id", "judul", "preview", "urutan", "link", "status", "aksi"],
+            lists: [
+              { id: 1, judul: 'Banner Input', preview: 'gambar', urutan: 1, link: 'https://google.com', status: true, aksi: 'Aksi'},
+              { id: 2, judul: 'Banner Input', preview: 'gambar', urutan: 2, link: 'https://google.com', status: false, aksi: 'Aksi'},
+              { id: 3, judul: 'Banner Input', preview: 'gambar', urutan: 3, link: 'https://google.com', status: false, aksi: 'Aksi'},
+              { id: 4, judul: 'Banner Input', preview: 'gambar', urutan: 4, link: 'https://google.com', status: false, aksi: 'Aksi'}
+            ],
+            dragging: false
+          };
     },
     components: { draggable }
 };
