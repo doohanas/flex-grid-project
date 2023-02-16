eb0132e6906caf1e870c898015b932e3d49763f8
Doohan <doohanaditya@yahoo.co.id>
2023-02-16 10:06:26 +0700

    feat (masonry): create masonry layout for image using column css

--- src/views/MasonryLayout.vue
+++ src/views/MasonryLayout.vue
@@ -1,5 +1,40 @@
-<template>
-  <div>
-    Test
+<template >
+  <div class="max-w-full h-full overflow-y-auto columns-3 md:columns-4 lg:columns-4 xl:columns-6 gap-[3px] image--column">
+     <img
+     class="pb-2 image-check"
+     v-for="item in 50" 
+     :key="item" 
+     :src="checkImg(item)" 
+     alt=""
+     >
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      landscapeImage: 'https://picsum.photos/500/300',
+      largeImage: 'https://picsum.photos/1200/600',
+      potraitImage: 'https://picsum.photos/300/300'
+    }
+  },
+  methods: {
+    checkImg (item) {
+      if (item % 2 === 0) {
+        return this.landscapeImage
+      } if (item % 3 === 0) {
+        return this.largeImage
+      } 
+        return this.potraitImage
+    }
+  }
+}
+</script>
+
+<style scoped>
+.image--column {
+  column-fill: balance;
+}
+
+</style>
